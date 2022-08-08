61f358a705f5374c92c5a67091b8bb09a2902279
jonssonyan <yz808@outlook.com>
2022-08-08 09:53:43 +0800

    fix: compile

--- compile-hysteria.bat
+++ compile-hysteria.bat
@@ -2,29 +2,29 @@
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=386
-go build -o build/hysteria-linux/386 -trimpath -ldflags "-s -w -buildid="
+go build -o build/hysteria-linux/386 -trimpath -ldflags "-s -w -buildid=" ./cmd
 ::Linux amd64
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
-go build -o build/hysteria-linux/amd64 -trimpath -ldflags "-s -w -buildid="
+go build -o build/hysteria-linux/amd64 -trimpath -ldflags "-s -w -buildid=" ./cmd
 ::Linux arm
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=arm
-go build -o build/hysteria-linux/arm -trimpath -ldflags "-s -w -buildid="
+go build -o build/hysteria-linux/arm -trimpath -ldflags "-s -w -buildid=" ./cmd
 ::Linux arm64
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=arm64
-go build -o build/hysteria-linux/arm64 -trimpath -ldflags "-s -w -buildid="
+go build -o build/hysteria-linux/arm64 -trimpath -ldflags "-s -w -buildid=" ./cmd
 ::Linux ppc64le
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=ppc64le
-go build -o build/hysteria-linux/ppc64le -trimpath -ldflags "-s -w -buildid="
+go build -o build/hysteria-linux/ppc64le -trimpath -ldflags "-s -w -buildid=" ./cmd
 ::Linux s390x
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=s390x
-go build -o build/hysteria-linux/s390x -trimpath -ldflags "-s -w -buildid="
+go build -o build/hysteria-linux/s390x -trimpath -ldflags "-s -w -buildid=" ./cmd
