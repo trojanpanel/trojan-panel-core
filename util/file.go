eb95d6749223c71736369f743129f2f645e8f70e
jonssonyan <yz808@outlook.com>
2022-08-08 16:38:02 +0800

    feat: core

--- util/file.go
+++ util/file.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"io"
+	"io/fs"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -25,7 +26,7 @@ func DownloadFile(url string, fileName string) error {
 	if err != nil {
 		return err
 	}
-	if err = ioutil.WriteFile(fileName, data, 0644); err != nil {
+	if err = ioutil.WriteFile(fileName, data, fs.ModePerm); err != nil {
 		return err
 	}
 	return nil
