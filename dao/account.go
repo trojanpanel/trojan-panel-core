47e0b8327208bdce261812b2408a327237180fb9
jonssonyan <yz808@outlook.com>
2022-08-11 01:00:31 +0800

    fix: core

--- dao/account.go
+++ dao/account.go
@@ -7,9 +7,9 @@ import (
 	"trojan-panel-core/module/constant"
 )
 
-func UpdateAccountById(id int, quota int) error {
+func UpdateAccountById(id int, download int, upload int) error {
 	where := map[string]interface{}{"id": id}
-	update := map[string]interface{}{"quota": quota}
+	update := map[string]interface{}{"download": download, "upload": upload}
 	buildUpdate, values, err := builder.BuildUpdate(mySQLConfig.AccountTable, where, update)
 	if err != nil {
 		logrus.Errorln(err.Error())
