a6834a24c1eaa2719516e4d5b29cc47c1c55f228
jonssonyan <yz808@outlook.com>
2022-08-23 15:07:39 +0800

    fix: core

--- module/constant/error.go
+++ module/constant/error.go
@@ -7,11 +7,8 @@ const (
 
 	GrpcError          string = "gRPC连接异常"
 	XrayStartError     string = "启动Xray失败"
-	XrayNotStart       string = "未启动Xray"
 	TrojanGoStartError string = "启动TrojanGo失败"
-	TrojanGoNotStart   string = "未启动TrojanGo"
 	HysteriaStartError string = "启动Hysteria失败"
-	HysteriaNotStart   string = "未启动Hysteria"
 	ProcessStopError   string = "进程暂停失败"
 
 	DownloadFilError   string = "远程文件下载失败"
