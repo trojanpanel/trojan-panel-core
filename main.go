2ec572fd68f7794aa7573a472776d58e51c5174f
jonssonyan <yz808@outlook.com>
2022-08-30 15:52:04 +0800

    fix: core

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/xtls/xray-core/common/net"
+	"google.golang.org/grpc"
 	"trojan-panel-core/api"
 	"trojan-panel-core/core"
 	"trojan-panel-core/dao"
@@ -14,6 +17,14 @@ func main() {
 	r := gin.Default()
 	router.Router(r)
 	_ = r.Run(":8082")
+
+	rpcServer := grpc.NewServer()
+	api.RegisterApiNodeServiceServer(rpcServer, new(api.ServerApi))
+	listener, err := net.Listen("tcp", ":8100")
+	if err != nil {
+		panic(fmt.Sprintf("gRPC服务监听端口失败%v", err))
+	}
+	_ = rpcServer.Serve(listener)
 }
 
 func init() {
