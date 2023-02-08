fa37002341cd9e00d7fdd343cfea4a04b6ed8f0f
jonssonyan <yz808@outlook.com>
2023-02-08 23:38:19 +0800

    feat: v2.0.2

--- dao/node_config.go
+++ dao/node_config.go
@@ -2,22 +2,44 @@ package dao
 
 import (
 	"errors"
+	"fmt"
 	"trojan-panel-core/module"
 	"trojan-panel-core/module/constant"
 )
 
-func SelectNodeConfigByNodeTypeIdAndApiPort(apiPort uint, nodeTypeId uint) (*module.NodeConfig, error) {
+func SelectNodeConfigByNodeTypeIdAndApiPort(apiPortParam uint, nodeTypeIdParam uint) (*module.NodeConfig, error) {
 	stmt, err := sqliteDb.Prepare("select id,node_type_id,api_port,protocol,xray_flow,xray_ss_method from node_config where api_port = ? and node_type_id = ?")
 	if err != nil {
 		return nil, errors.New(constant.SysError)
 	}
-	row := stmt.QueryRow(apiPort, nodeTypeId)
-	if row.Err() != nil {
+	rows, err := stmt.Query(apiPortParam, nodeTypeIdParam)
+	if err != nil || rows.Err() != nil {
 		return nil, errors.New(constant.SysError)
 	}
-	var nodeConfig module.NodeConfig
-	if err := row.Scan(&nodeConfig); err != nil {
-		return nil, errors.New(constant.SysError)
+	defer rows.Close()
+
+	var (
+		id           uint
+		apiPort      uint
+		nodeTypeId   uint
+		protocol     string
+		xrayFlow     string
+		xraySSMethod string
+	)
+	for rows.Next() {
+		if err := rows.Scan(&id, &apiPort, &nodeTypeId, &protocol, &xrayFlow, &xraySSMethod); err != nil {
+			fmt.Println(err.Error())
+			return nil, errors.New(constant.SysError)
+		}
+		break
+	}
+	nodeConfig := module.NodeConfig{
+		Id:           id,
+		ApiPort:      apiPort,
+		NodeTypeId:   nodeTypeId,
+		Protocol:     protocol,
+		XrayFlow:     xrayFlow,
+		XraySSMethod: xraySSMethod,
 	}
 	return &nodeConfig, nil
 }
