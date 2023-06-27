225217154a82ee2184b5ebd0fa769c10078e13bd
jonssonyan <yz808@outlook.com>
2023-06-27 19:04:27 +0800

    fix: redis lock

--- service/account.go
+++ service/account.go
@@ -299,7 +299,7 @@ func CronHandlerDownloadAndUpload() {
 							continue
 						}
 					}
-					defer redis.RsUnLock(mutex)
+					redis.RsUnLock(mutex)
 				}(stats)
 			}
 			return true
@@ -342,7 +342,7 @@ func CronHandlerDownloadAndUpload() {
 							continue
 						}
 					}
-					defer redis.RsUnLock(mutex)
+					redis.RsUnLock(mutex)
 				}(users)
 			}
 			return true
