616e75fb69a7911fe5f6459c9249585f82dcd9ba
jonssonyan <yz808@outlook.com>
2025-03-17 17:53:46 +0800

    fix: proxy

--- go.mod
+++ go.mod
@@ -7,28 +7,27 @@ require (
 	github.com/gin-gonic/gin v1.8.2
 	github.com/go-playground/validator/v10 v10.11.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/google/go-github/v39 v39.2.0
 	github.com/redis/go-redis/v9 v9.6.1
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/cobra v1.8.1
-	github.com/xtls/xray-core v1.8.0
-	google.golang.org/grpc v1.53.0
-	google.golang.org/protobuf v1.29.0
+	github.com/xtls/xray-core v1.250306.0
+	google.golang.org/grpc v1.71.0
+	google.golang.org/protobuf v1.36.5
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
 
 require (
 	github.com/BurntSushi/toml v1.2.0 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/go-github/v39 v39.2.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -42,10 +41,10 @@ require (
 	github.com/tklauser/numcpus v0.6.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- proxy/xray_api.go
+++ proxy/xray_api.go
@@ -1,1 +1,118 @@
 package proxy
+
+import (
+	"context"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/xtls/xray-core/app/proxyman/command"
+	statscmd "github.com/xtls/xray-core/app/stats/command"
+	"github.com/xtls/xray-core/common/protocol"
+	"github.com/xtls/xray-core/common/serial"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"strings"
+	"time"
+	"trojan-core/model/bo"
+	"trojan-core/model/constant"
+)
+
+type XrayApi struct {
+	apiPort string
+}
+
+func NewXrayApi(apiPort string) *XrayApi {
+	return &XrayApi{
+		apiPort: apiPort,
+	}
+}
+
+func apiClient(apiPort string) (conn *grpc.ClientConn, ctx context.Context, clo func(), err error) {
+	conn, err = grpc.Dial(fmt.Sprintf("127.0.0.1:%s", apiPort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	clo = func() {
+		cancel()
+		if conn != nil {
+			_ = conn.Close()
+		}
+	}
+	if err != nil {
+		logrus.Errorf("xray apiClient init err: %v", err)
+		err = fmt.Errorf(constant.GrpcError)
+	}
+	return
+}
+
+func (x *XrayApi) QueryStats(pattern string, reset bool) ([]bo.XrayStatsBo, error) {
+	conn, ctx, clo, err := apiClient(x.apiPort)
+	defer clo()
+	if err != nil {
+		return nil, err
+	}
+	statsServiceClient := statscmd.NewStatsServiceClient(conn)
+	response, err := statsServiceClient.QueryStats(ctx, &statscmd.QueryStatsRequest{
+		Pattern: pattern,
+		Reset_:  reset,
+	})
+	if err != nil {
+		logrus.Errorf("xray QueryStats err: %v", err)
+		return nil, fmt.Errorf(constant.GrpcError)
+	}
+	stats := response.GetStat()
+	var xrayStatsVos []bo.XrayStatsBo
+	for _, stat := range stats {
+		xrayStatsVos = append(xrayStatsVos, bo.XrayStatsBo{
+			Name:  stat.Name,
+			Value: int(stat.GetValue()),
+		})
+	}
+	return xrayStatsVos, nil
+}
+
+func (x *XrayApi) AddUser(password string, message *serial.TypedMessage) error {
+	conn, ctx, clo, err := apiClient(x.apiPort)
+	defer clo()
+	if err != nil {
+		return nil
+	}
+	handlerServiceClient := command.NewHandlerServiceClient(conn)
+	_, err = handlerServiceClient.AlterInbound(ctx, &command.AlterInboundRequest{
+		Tag: "user",
+		Operation: serial.ToTypedMessage(&command.AddUserOperation{
+			User: &protocol.User{
+				Email:   password,
+				Level:   0,
+				Account: serial.ToTypedMessage(message),
+			},
+		}),
+	})
+	if err != nil {
+		if strings.HasSuffix(err.Error(), "already exists.") {
+			return nil
+		}
+		logrus.Errorf("xray AddUser err: %v", err)
+		return fmt.Errorf(constant.GrpcError)
+	}
+	return nil
+}
+
+func (x *XrayApi) DeleteUser(email string) error {
+	conn, ctx, clo, err := apiClient(x.apiPort)
+	defer clo()
+	if err != nil {
+		return err
+	}
+	hsClient := command.NewHandlerServiceClient(conn)
+	_, err = hsClient.AlterInbound(ctx, &command.AlterInboundRequest{
+		Tag:       "user",
+		Operation: serial.ToTypedMessage(&command.RemoveUserOperation{Email: email}),
+	})
+	if err != nil {
+		if strings.HasSuffix(err.Error(), "not found.") {
+			return nil
+		}
+		logrus.Errorf("xray DeleteUser err: %v", err)
+		return fmt.Errorf(constant.GrpcError)
+	}
+	return nil
+}
