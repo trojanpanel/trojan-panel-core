85aca191294898ec7565671229e2f3c0c3cff25c
jonssonyan <yz808@outlook.com>
2025-03-18 17:01:56 +0800

    fix: test

--- tests/proxy/xray_test.go
+++ tests/proxy/xray_test.go
@@ -1,6 +1,7 @@
 package proxy
 
 import (
+	"encoding/json"
 	"testing"
 	"trojan-core/proxy"
 )
@@ -9,4 +10,13 @@ func TestDownloadXray(t *testing.T) {
 	if err := proxy.DownloadXray(""); err != nil {
 		println(err.Error())
 	}
+}
+
+func TestXrayQueryStats(t *testing.T) {
+	xrayApi := proxy.NewXrayApi("18080")
+	stats, err := xrayApi.QueryStats("", false)
+	if err != nil {
+		return
+	}
+	println(json.Marshal(stats))
 }
