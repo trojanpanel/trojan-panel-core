5bf72a6e4850383f7f2cf80aa4638454d1373908
jonssonyan <yz808@outlook.com>
2025-03-24 16:32:12 +0800

    fix: proxy api

--- tests/api/proxy_test.go
+++ tests/api/proxy_test.go
@@ -1,1 +1,46 @@
 package api
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+	"trojan-core/api/proxy"
+	"trojan-core/model/constant"
+)
+
+func TestStartProxy(t *testing.T) {
+	token := ""
+	conn, ctx, clo, err := newGrpcInstance(token, fmt.Sprintf("127.0.0.1:%s", os.Getenv(constant.GrpcPort)), 4*time.Second)
+	defer clo()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	client := proxy.NewApiProxyServiceClient(conn)
+	dto := proxy.StartProxyDto{}
+	send, err := client.StartProxy(ctx, &dto)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if send.Success {
+		fmt.Println(send.Data)
+	}
+}
+
+func TestStopProxy(t *testing.T) {
+	token := ""
+	conn, ctx, clo, err := newGrpcInstance(token, fmt.Sprintf("127.0.0.1:%s", os.Getenv(constant.GrpcPort)), 4*time.Second)
+	defer clo()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	client := proxy.NewApiProxyServiceClient(conn)
+	dto := proxy.StopProxyDto{}
+	send, err := client.StopProxy(ctx, &dto)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if send.Success {
+		fmt.Println(send.Data)
+	}
+}
